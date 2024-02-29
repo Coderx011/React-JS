71d9019278a0623e819b8fbaf8fc7c4b0b1615d7
Test Project <meharshani963@gmail.com>
2024-02-29 06:48:49 +0500

    1st comment

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Zeeshan ali</h1>
+  );
+}
+
+export default App;
---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>zeeshan Ali with vite</h1>
+  )
+}
+
+export default App
