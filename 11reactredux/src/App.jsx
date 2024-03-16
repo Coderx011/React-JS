8bdc10fd64350efb0ee86e9dc4445c1c96c70e34
Test Project <meharshani963@gmail.com>
2024-03-16 19:35:30 +0500

    final project

---
+++ 11reactredux/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './compnents/AddTodo'
+import Todos from './compnents/Todo'
+
+function App() {
+
+  return (
+    <>
+    <h1>hey</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
